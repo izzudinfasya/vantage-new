27960a20850fedf9f234b7a43bea8180b9149171
Izzudin Fasya <izzudinfasya@gmail.com>
2024-10-20 23:34:30 +0700

    update 20 oct

--- frontend/src/components/emailSubs.tsx
+++ frontend/src/components/emailSubs.tsx
@@ -17,8 +17,7 @@ const EmailSubscriptionModal: React.FC<EmailSubscriptionModalProps> = ({
       const values = await form.validateFields();
       setLoading(true);
 
-      console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
-      const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/vouchers/get-voucher`;
+      const apiUrl = `${process.env.REACT_APP_API_URL}/vouchers/get-voucher`;
 
       const response = await fetch(apiUrl, {
         method: "POST",
