157335d154dd7150429ca722022ffd28a133214b
Izzudin Fasya <izzudinfasya@gmail.com>
2024-10-26 01:30:08 +0700

    backend test

--- backend/dist/routes/subscriptionRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const subscriptionController_1 = require("../controllers/subscriptionController");
-const router = (0, express_1.Router)();
-router.post("/", subscriptionController_1.createSubscription);
-exports.default = router;
