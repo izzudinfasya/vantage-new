b13d47351d3feb4ab8afd2c69a5fa8d200e674f8
Izzudin Fasya <izzudinfasya@gmail.com>
2024-11-01 12:31:56 +0700

    update 01 nov

--- frontend/src/pages/passwordPage.tsx
+++ frontend/src/pages/passwordPage.tsx
@@ -124,13 +124,11 @@ const PasswordPage: React.FC<PasswordPageProps> = ({ onLogin }) => {
 
   return (
     <div className="password-page">
-      {isLoading && (
-        <div className={`loading-overlay ${isLoading ? "" : "hidden"}`}>
-          <div className="loading-content">
-            <img src={gifAvatar} alt="Loading" className="loading-logo" />
-          </div>
+      <div className={`loading-overlay ${isLoading ? "" : "hidden"}`}>
+        <div className="loading-content">
+          <img src={gifAvatar} alt="Loading" className="loading-logo" />
         </div>
-      )}
+      </div>
 
       <div className="avatar-container">
         <img src={gifAvatar} alt="Avatar" className="avatar-gif" />
